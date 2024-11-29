03b8a86d4067db1aceffce447926b6cd96c32c27
gxnie <wlsdl7520@naver.com>
2024-11-29 13:52:17 +0900

    feat: Get, Put, Delete 기능 추가
    - 고유 식별자로 특정 고객 환전 요청 조회
    - 환전 요청 상태 취소로 변경
    - 모든 환전 요청 삭제

--- src/main/java/com/sparta/currency_user/controller/ExchangeController.java
+++ src/main/java/com/sparta/currency_user/controller/ExchangeController.java
@@ -2,12 +2,10 @@ package com.sparta.currency_user.controller;
 
 import com.sparta.currency_user.dto.ExchangeRequestDto;
 import com.sparta.currency_user.dto.ExchangeResponseDto;
+import com.sparta.currency_user.dto.ExchangeStatusDto;
 import com.sparta.currency_user.service.ExchangeService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/exchange")
@@ -26,15 +24,33 @@ public class ExchangeController {
         // user id(사용자 id), currency id(통화 id), AmountInKrw(환전 전 금액)
     }
 
-
     // R -> 고객 고유 식별자(User_id)로 특정 고객 환전 요청 조회
-
+    @GetMapping("/{id}")
+    public ResponseEntity<ExchangeResponseDto> findExchange(@PathVariable Long id){
+        return ResponseEntity.ok().body(exchangeService.findById(id));
+    }
 
     // U -> 환전 요청 상태를 취소로 변경
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> updateExchange(@PathVariable Long id,
+                             @RequestBody ExchangeStatusDto exchangeStatusDto){
 
+        // 환전 요청 상태를 업데이트하는 서비스
+        exchangeService.updateExchangeById(id,exchangeStatusDto);
+
+        // 환전 요청 상태가 정상적으로 업데이트되었을 때 메세지
+        return ResponseEntity.ok().body("정상적으로 업데이트 되었습니다.");
+    }
 
     // D -> 고객 삭제 + 모든 환전 요청도 삭제
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteExchange(@PathVariable Long id){
+        // 고객의 환전 요청을 삭제
+        exchangeService.deleteExchangeById(id);
 
+        // 삭제가 정상적으로 되었을 때 메세지
+        return ResponseEntity.ok().body("정상적으로 삭제되었습니다.");
+    }
 
 
 
