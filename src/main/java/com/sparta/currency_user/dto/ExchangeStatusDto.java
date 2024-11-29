391911fdc2b53833044d329b1d1b09784a3c09c5
gxnie <wlsdl7520@naver.com>
2024-11-29 13:47:51 +0900

    feat: ExchangeStatusDto 생성
    환전 상태 수정 관련 Dto

---
+++ src/main/java/com/sparta/currency_user/dto/ExchangeStatusDto.java
@@ -0,0 +1,13 @@
+package com.sparta.currency_user.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ExchangeStatusDto {
+
+    private String status;
+
+}
